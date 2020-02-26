2e1d0202934a5d1a2bf42d137234083a37b5e397
rumesh986 <rumesh986@gmail.com>
2020-02-27 00:30:38 +0800

    new script that helped in checking if mapping was completed on the pi side

--- listener.py
+++ listener.py
@@ -38,9 +38,14 @@
 
 import rospy
 from std_msgs.msg import String
+from std_msgs.msg import UInt16MultiArray
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    #rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    #rospy.loginfo(data)
+    rospy.loginfo(data.data)
+    rospy.loginfo(data.data[0])
+    rospy.loginfo(data.data[1])
 
 def listener():
 
@@ -51,7 +56,7 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber('chatter', String, callback)
+    rospy.Subscriber('target_loc', UInt16MultiArray, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
